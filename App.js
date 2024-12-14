6fdc3aedb04b35cbd754dc1daca5c31ce818fba6
higashi <haruka.higashi@trunk-net.co.jp>
2024-12-14 13:00:55 +0900

    20241214

--- App.js
+++ App.js
@@ -11,54 +11,54 @@ import MemoList from './src/screens/MemoList'; // メモ一覧画面
 import MemoEdit from './src/screens/MemoEdit'; // メモ編集画面
 
 const Stack = createNativeStackNavigator();
-const customDarkTheme = {
-  ...MD3DarkTheme,
-  colors: {
-    ...MD3DarkTheme.colors,
-    primary: '#ffffff', // プライマリカラー
-    background: '#000000', // 背景色
-    surface: '#000000', // サーフェイスの色
-    onSurface: '#ffffff', // サーフェイス上のテキスト・アイコンの色
-  },
-};
+// const customDarkTheme = {
+//   ...MD3DarkTheme,
+//   colors: {
+//     ...MD3DarkTheme.colors,
+//     primary: '#ffffff', // プライマリカラー
+//     background: '#000000', // 背景色
+//     surface: '#000000', // サーフェイスの色
+//     onSurface: '#ffffff', // サーフェイス上のテキスト・アイコンの色
+//   },
+// };
 
 export default function App() {
   return (
-    <PaperProvider>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name='Login' component={Login} options={{ headerShown: false }} />
-          <Stack.Screen
-            name='MemoList'
-            component={MemoList}
-            options={{
-              title: 'メモ一覧',
-              headerStyle: {
-                backgroundColor: '#b0e0e6',
-              },
+    // <PaperProvider>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Login' component={Login} options={{ headerShown: false }} />
+        <Stack.Screen
+          name='MemoList'
+          component={MemoList}
+          options={{
+            title: 'メモ一覧',
+            headerStyle: {
+              backgroundColor: '#b0e0e6',
+            },
 
-              headerTintColor: '#555',
-              headerTitleStyle: {
-                fontWeight: 'bold',
-              },
-              headerBackVisible: false,
-              gestureEnabled: false,
-              headerTitleAlign: 'center',
-            }}
-          />
-          <Stack.Screen
-            name='MemoEdit'
-            component={MemoEdit}
-            options={{
-              title: '',
-              headerStyle: {
-                backgroundColor: '#b0e0e6',
-              },
-            }}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>{' '}
-    </PaperProvider>
+            headerTintColor: '#555',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+            headerBackVisible: false,
+            gestureEnabled: false,
+            headerTitleAlign: 'center',
+          }}
+        />
+        <Stack.Screen
+          name='MemoEdit'
+          component={MemoEdit}
+          options={{
+            title: '',
+            headerStyle: {
+              backgroundColor: '#b0e0e6',
+            },
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+    // </PaperProvider>
   );
 }
 
